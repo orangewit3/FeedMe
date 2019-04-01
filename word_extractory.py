2238b190809bb33a5438378916a51761ebc0b5e4
akhiljakatdar <akhilrj2@illinois.edu>
2019-04-01 15:10:41 -0500

    Updated word_extractory

--- word_extractory.py
+++ word_extractory.py
@@ -1,6 +1,10 @@
 from newsplease import NewsPlease
 from googlesearch import search_news
+import urllib.request
+import http.client
+import httplib2
 import time
+import readability
 
 new_list = ['en.wikipedia.org', 'twitter.com']
 '''
@@ -17,9 +21,17 @@ def article_generator(keyword_query, num_articles):
 def article_generator_text(keyword_query, num_articles):
     text = ''
     for url in search_news(str(keyword_query), num=1, stop=num_articles):
-        article = NewsPlease.from_url(str(url))
-        if (article.text != None):
+        try:
+            article = NewsPlease.from_url(str(url))
+        except Exception as e:
+            continue
+        except urllib.request.HTTPError as e:
+            continue
+        except http.client.HTTPException as e:
+            continue
+        except UnicodeDecodeError as e:
+            continue
+        if (article.text != None and len(article.text) > 100):
             if article.source_domain not in new_list:
                 text = text + article.text
     return text
-
